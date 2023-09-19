edb9026e65d0f4124b9a0f206e063b9b11fc0698
Jairo Koning <jairo.koning@gmail.com>
2023-09-19 11:32:12 -0300

    feat: create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,34 @@
+import Image from 'next/image'
+import { HomeContainer, Product } from "@/styles/pages/home";
+
+import tshirt1 from '../assets/t-shirts/tshirt1.png'
+import tshirt2 from '../assets/t-shirts/tshirt2.png'
+import tshirt3 from '../assets/t-shirts/tshirt3.png'
+
 export default function Home() {
   return (
-    <h1>Hello world</h1>   
+    <HomeContainer>
+      <Product>
+        <Image src={tshirt1} width={520} height={480} alt="" />
+        <footer>
+          <strong>T-shirt 1</strong>
+          <span>$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={tshirt2} width={520} height={480} alt="" />
+        <footer>
+          <strong>T-shirt 2</strong>
+          <span>$ 79,90</span>
+        </footer>
+      </Product>
+      {/* <Product>
+        <Image src={tshirt3} width={520} height={480} alt="" />
+        <footer>
+          <strong>T-shirt 3</strong>
+          <span>$ 79,90</span>
+        </footer>
+      </Product> */}
+    </HomeContainer>
   )
 }
