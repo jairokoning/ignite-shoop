041497543be78ce9e84fad58548ce808e1ea0328
Jairo Koning <jairo.koning@gmail.com>
2023-09-21 10:26:21 -0300

    feat: add link navigation between pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { useKeenSlider } from 'keen-slider/react'
 import { HomeContainer, Product } from "@/styles/pages/home";
 
 import 'keen-slider/keen-slider.min.css'
+import Link from 'next/link';
 
 interface HomeProps {
   products: {
@@ -28,13 +29,15 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className='keen-slider'>
       {products.map(product => {
         return (
-          <Product key={product.id} className='keen-slider__slide'>
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product className='keen-slider__slide'>
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
