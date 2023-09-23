7776d7f1fb3b5d6d3046bb343742761e33f62803
Jairo Koning <jairo.koning@gmail.com>
2023-09-22 21:34:19 -0300

    feat: add product page layout

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,3 +1,18 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product";
+
 export default function Product() {
-  return <h1>sdf</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>$ 79.90</span>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, tempore a! Libero et doloribus debitis adipisci necessitatibus saepe repellat explicabo labore! Vel doloribus excepturi maxime hic, culpa pariatur reprehenderit! Itaque!</p>
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
