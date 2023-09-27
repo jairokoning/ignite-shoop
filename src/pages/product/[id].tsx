3aaf19f62124df6f103831e5e0b732af9e322cff
Jairo Koning <jairo.koning@gmail.com>
2023-09-27 15:24:08 -0300

    feat: stripe checkout

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -12,6 +12,7 @@ interface ProductProps {
     imageUrl: string
     price: string
     description: string
+    defaultPriceId: string
   }
 }
 
@@ -67,6 +68,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
           currency: 'USD',
         }).format(price.unit_amount! / 100),
         description: product.description,
+        defaultPriceId: price.id,
       }
     },
     revalidate: 60 * 60 * 1, //  1 hour
