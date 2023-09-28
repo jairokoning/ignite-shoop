f301f198211da308355bb8d193bf06a5a7eb1a14
Jairo Koning <jairo.koning@gmail.com>
2023-09-28 14:45:49 -0300

    feat: page success layout

---
+++ src/pages/success.tsx
@@ -0,0 +1,22 @@
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
+export default function Success() {
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhuul <strong>Jairo Koning</strong>, sua <strong>Camiseta</strong> já está a caminho da sua casa.
+      </p>
+
+      <Link href="/">
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
+  )
+}
